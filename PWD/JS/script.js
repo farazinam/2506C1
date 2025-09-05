ebf7e765b155fa0613d45c6c5e5bc0e2523c93d1
farazinam <farazinam17@gmail.com>
2025-09-05 13:09:47 +0500

    JavaScript Day 1 - Varibales & DataTypes

---
+++ PWD/JS/script.js
@@ -0,0 +1,50 @@
+document.write("Hello World!");
+
+document.write("<br>");
+// alert("Hello World!");
+// console.log("Hello world!");
+
+// variables
+
+// var a = 5;
+// document.write(a);
+
+// var name = 'Ali'; //string
+// document.write(name);
+
+name = "Faraz";
+// document.write(name);
+
+// document.write(name);
+
+document.write("My Name is " + name);
+
+document.write(30 + 20);
+var num1 = 22;
+var num2 = 7;
+// document.write(num1 + num2)
+document.write("The Addition of Number 1 and Number 2 is " + (num1 + num2));
+
+// var, let, const
+
+var v = 5;
+let l = 10;
+const c = 15;
+
+var v = 10;
+l = 3;
+// c = 20;
+
+document.write(v);  //10
+document.write(l);  //3
+document.write(c);  //15
+
+
+// Varibale naming Rule
+lab = 10;
+MyName = "Ali";
+My_Name_is = "Ali";
+m4yname8 = "Abc";
+_123 = "hfghj";
+$12hjgfh = "hjsh";
+
